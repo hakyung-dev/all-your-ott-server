8e2dcb0982d25cf02c2a99cdfbabe32f2109ce9d
hakyung <hakyung.dev@gmail.com>
2020-11-13 18:29:00 +0900

    Replace bcrypt with bcryptjs

--- routes/middlewares/userValidation.js
+++ routes/middlewares/userValidation.js
@@ -1,5 +1,5 @@
 const User = require('../../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.signUpValidation = async (req, res, next) => {
   const { email } = req.body;
